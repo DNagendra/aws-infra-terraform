f415292c37f84e564d1d2c0b30afca52f5e89dcc
DNagendra <61675246+DNagendra@users.noreply.github.com>
2020-04-17 13:47:20 -0400

    updated code

---
+++ aws-eks-infra/eks-cluster.tf
@@ -0,0 +1,100 @@
+Skip to content
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@DNagendra 
+DNagendra
+/
+terraform-provider-aws
+forked from terraform-providers/terraform-provider-aws
+0
+0
+3.8k
+ Code Pull requests 0 Actions Projects 0 Wiki Security Insights Settings
+terraform-provider-aws/examples/eks-getting-started/eks-cluster.tf
+ dnagendra updated with new values
+134b28f 21 hours ago
+@bflad@anthonydahanne@rachlenko
+77 lines (66 sloc)  1.97 KB
+  
+#
+# EKS Cluster Resources
+#  * IAM Role to allow EKS service to manage other AWS services
+#  * EC2 Security Group to allow networking traffic with EKS cluster
+#  * EKS Cluster
+#
+
+resource "aws_iam_role" "eks-cluster" {
+  name = "terraform-eks-cluster"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "eks-cluster-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks-cluster.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks-cluster-AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks-cluster.name
+}
+
+resource "aws_security_group" "eks-cluster" {
+  name        = "terraform-eks-cluster"
+  description = "Cluster communication with worker nodes"
+  vpc_id      = aws_vpc.infi-vpc.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "terraform-eks-cluster"
+  }
+}
+
+resource "aws_security_group_rule" "eks-cluster-ingress-workstation-https" {
+  cidr_blocks       = [local.workstation-external-cidr]
+  description       = "Allow workstation to communicate with the cluster API Server"
+  from_port         = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.eks-cluster.id
+  to_port           = 443
+  type              = "ingress"
+}
+
+resource "aws_eks_cluster" "eks-cluster" {
+  name     = var.cluster-name
+  role_arn = aws_iam_role.eks-cluster.arn
+
+  vpc_config {
+    security_group_ids = [aws_security_group.eks-cluster.id]
+    subnet_ids         = aws_subnet.infisubnet[*].id
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks-cluster-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.eks-cluster-AmazonEKSServicePolicy,
+  ]
+}
